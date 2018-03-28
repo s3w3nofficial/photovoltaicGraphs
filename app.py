65528d912a854d129669a3b65b63b3121a3970eb
Martin Urbanec <martin@urbanec.cz>
2018-03-28 10:36:01 +0200

    Implement downloading

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, jsonify
-from datetime import datetime
+from flask import Flask, render_template, jsonify, request
+from datetime import datetime, timedelta
 import pandas as pd
+import tempfile
+import shutil
+import requests
+import os
 
 app = Flask(__name__)
 
@@ -8,8 +12,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-def downloadlocaldata(url, datestart, dateend):
-    pass
+def fetchlocaldata(urlTmpl, format, datestart, dateend):
+    # Returns directory name with fetched local data. Caller is responsible for its deletion.
+    date = datestart
+    delta = timedelta(days=1)
+    basepath = tempfile.mkdtemp()
+    while date < dateend:
+        filename = date.strftime(format)
+        url = '%s/%s' % (urlTmpl, filename)
+        r = requests.get(url, stream=True)
+        local_filepath = os.path.join(basepath, filename)
+        with open(local_filepath, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        date += delta
+    return basepath
 
 @app.route('/graph', methods=['POST', 'GET'])
 def showGraph():
